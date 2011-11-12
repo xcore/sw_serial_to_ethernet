fd7dddb0d9d01366111fe34538d76d881fa8a6f8
Paul Hampson <paul@xmos.com>
2011-11-12 15:07:16 +0000

    some tidy up

--- app_multi_uart_demo/.build/sc_multi_uart/module_multi_uart_tx/src/multi_uart_api.c.d
+++
@@ -1,4 +0,0 @@
-.build/sc_multi_uart/module_multi_uart_tx/src/multi_uart_api.c.d ./.build/sc_multi_uart/module_multi_uart_tx/src/multi_uart_api.c.o:  \
- ./../../sc_multi_uart/module_multi_uart_tx/src/multi_uart_api.c \
-  ./../../sc_multi_uart/module_multi_uart_tx/src/multi_uart_tx.h \
-  ././src/multi_uart_tx_conf.h
--- app_multi_uart_demo/.build/sc_multi_uart/module_multi_uart_tx/src/multi_uart_tx.xc.d
+++
@@ -1,5 +0,0 @@
-.build/sc_multi_uart/module_multi_uart_tx/src/multi_uart_tx.xc.d ./.build/sc_multi_uart/module_multi_uart_tx/src/multi_uart_tx.xc.o: \
-  ./../../sc_multi_uart/module_multi_uart_tx/src/multi_uart_tx.xc \
-  ../../sc_multi_uart/module_multi_uart_tx/src/multi_uart_tx.h \
-  ../../sc_multi_uart/app_multi_uart_demo/src/multi_uart_tx_conf.h
-
