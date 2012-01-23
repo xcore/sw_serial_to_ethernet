a65839b31d4e73bb56a9d4ee745db00488eb5f37
Paul Hampson <paul@xmos.com>
2012-01-23 11:02:59 +0000

    removed defaults for configuration - will now get an error

--- module_multi_uart/src/rx/multi_uart_rx.h
+++ module_multi_uart/src/rx/multi_uart_rx.h
@@ -13,30 +13,7 @@
 #ifdef __multi_uart_rx_conf_h_exists__
 #include "multi_uart_rx_conf.h"
 #else
-
-/**
- * Define the external clock rate
- */
-#define UART_RX_CLOCK_RATE_HZ      1843200
-
-/**
- * Clock divider value that defines max baud rate. For external 1.8432MHz clock
- * Div 16 => 115200 max bps
- * Div 8  => 230400 max bps
- * Div 4  => 460800 max bps
- */
-#define UART_RX_CLOCK_DIVIDER      8
-
-/**
- * Define the buffer size in bytes - full UART words are stored in this buffer, so if 2 bytes per word are required (e.g. 12 bit UART word) then this must be accounted for
- */
-#define UART_RX_BUF_SIZE    16
-
-/**
- * Define the number of channels that are to be supported
- */
-#define UART_RX_CHAN_COUNT  8
-
+#error "No RX UART Configuration file"
 #endif /* __multi_uart_rx_conf_h_exists__ */
 
 /* define channel commands */
--- module_multi_uart/src/tx/multi_uart_tx.h
+++ module_multi_uart/src/tx/multi_uart_tx.h
@@ -13,43 +13,7 @@
 #ifdef __multi_uart_tx_conf_h_exists__
 #include "multi_uart_tx_conf.h"
 #else
-
-/**
- * Define to use external clock reference
- */
-#define UART_TX_USE_EXTERNAL_CLOCK
-
-/**
- * Define the external clock rate
- */
-#define UART_CLOCK_RATE_HZ      1843200
-
-/**
- * Define the max baud rate - validated to 230kbaud
- */
-#define UART_TX_MAX_BAUD_RATE   100000
-
-/**
- * Clock divider value that defines max baud rate - this is only used when using the internal clock
- */
-#define UART_TX_CLOCK_DIVIDER      (UART_TX_CLOCK_RATE_HZ/UART_TX_MAX_BAUD_RATE)
-
-/**
- * Define the oversampling of the clock - this is where the UART_TX_CLOCK_DIVIDER is > 255 
- * (otherwise set to 1) - only used when using an internal clock reference
- */
-#define UART_TX_OVERSAMPLE          2
-
-/**
- * Define the buffer size in bytes - full UART words are stored in this buffer, so if 2 bytes per word are required (e.g. 12 bit UART word) then this must be accounted for
- */
-#define UART_TX_BUF_SIZE    16
-
-/**
- * Define the number of channels that are to be supported
- */
-#define UART_TX_CHAN_COUNT  8
-
+#error "No UART TX configuration header file"
 #endif /* __multi_uart_tx_conf_h_exists__ */
 
 #ifdef __XC__
