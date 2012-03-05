67f751825d64e78dc6195440e8af67d0850bb8f8
Vinith Mundhra <vinith@xmos.com>
2012-03-05 16:47:48 +0530

    webpage to include changeable baud rates

--- app_serial_to_ethernet_demo/src/fs/flash.bat
+++ app_serial_to_ethernet_demo/src/fs/flash.bat
@@ -1,2 +1,2 @@
-:: xflash --id 0 --target-file XC-2.xn --erase-all
-xflash --target XC-2 --boot-partition-size 0x20000 --data flash.img app_simple_webserver.xe
+xflash --id 0 --target-file XS1-L2A-QF124.xn --erase-all
+xflash --id 0 --target-file XS1-L2A-QF124.xn --boot-partition-size 0x20000 --data flash.img app.xe
