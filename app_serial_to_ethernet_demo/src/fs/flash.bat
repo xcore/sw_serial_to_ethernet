b3def15edbe5a0288dd203ca6723d838b42cfaaf
Dave Lacey <david.lacey@xmos.com>
2012-06-19 07:39:56 +0100

    Moved over to new s2e app as default

--- app_serial_to_ethernet_demo/src/fs/flash.bat
+++
@@ -1,6 +0,0 @@
-:: Run makeFlash.pl to generate webpage image
-:: makeFlash.pl wpage
-del *.xe
-copy ..\..\bin\release\s2e_demo.xe
-xflash --id 0 --target-file xp-skc-l2-single.xn --erase-all
-xflash --id 0 --target-file xp-skc-l2-single.xn --boot-partition-size 0x20000 --data webpage_bin.img s2e_demo.xe
